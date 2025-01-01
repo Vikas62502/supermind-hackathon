d2dbe5115b8a86b01107e924ff15402cea7a1e82
Sujeet Kumar <sujeet@cuvette.tech>
2025-01-01 23:00:15 +0530

    sujeet: test route added

--- src/router/router.ts
+++ src/router/router.ts
@@ -5,5 +5,8 @@ import { dbController } from "../controller/controller";
 const router = express.Router();
 
 router.get("/db", dbController)
+router.get("/test", (req, res) => {
+    res.send("Hello World")
+})
 
 export { router };

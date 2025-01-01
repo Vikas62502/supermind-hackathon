3aae66a23fe8f44bf43fa5d051a14e3eb5c60e96
Vikas Yadav <vikashyadav62502@gmail.com>
2025-01-01 22:09:39 +0530

    Update index.ts
    astra db connection done

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import { DataAPIClient } from "@datastax/astra-db-ts";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
+
+// Initialize the client
+const client = new DataAPIClient(process.env.ASTRA_DB_TOKEN);
+const db = client.db(process.env.ASTRA_DB_URI!);
+
+(async () => {
+    const colls = await db.listCollections();
+    console.log('Connected to AstraDB:', colls);
+})();
 
 app.get("/", (req: Request, res: Response) => {
     res.send("Basic Express TypeScript Server");
